98b677a6a9822cfbd16f6daf66344013efb5cf40
kesapevv <vadimvoronkoffn@gmail.com>
2024-06-22 02:25:07 +0700

    ewf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
-///import Header from "./components/Header/Header";
-
-const tg = window.Telegram.WebApp;
+import Header from "./components/Header/Header";
 
 function App() {
 
-    /// const {onToggleButton, tg} = useTelegram();
+     const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
@@ -15,12 +13,10 @@ function App() {
 
     return (
         <div className="App">
-           hello
+            <Header />
+            <button onClick={onToggleButton}>toggle</button>
         </div>
     );
 }
 
 export default App;
-
-// <Header />
-// <button onClick={onToggleButton}>toggle</button>
